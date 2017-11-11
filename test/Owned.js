d79a8c8cfb836f246e9e3b11b8ca46c1e0f9dc51
Fabio <fabio.padoin@gmail.com>
2017-11-11 14:22:18 +0100

    General fixes

--- test/Owned.js
+++ test/Owned.js
@@ -2,13 +2,11 @@
  * Created by antoniogiordano on 26/10/2017.
  */
 
-const nope = () => null
+const Web3 = require('web3');
+const web3 = new Web3();
 
-const Web3 = require('web3')
-const web3 = new Web3()
-
-const TestRPC = require('ethereumjs-testrpc')
-web3.setProvider(TestRPC.provider())
+const TestRPC = require('ethereumjs-testrpc');
+web3.setProvider(TestRPC.provider());
 
 const Promise = require('bluebird');
 Promise.promisifyAll(web3.eth, {suffix: "Promise"});
@@ -27,55 +25,58 @@ SafeMath.setProvider(web3.currentProvider);
 Owned.setProvider(web3.currentProvider);
 
 describe("Owned tests", function () {
-    var accounts, networkId, owned, safeMath
+    var accounts, networkId, owned, safeMath;
+    var owner, attacker, testAccount;
 
-    before("get accounts", function () {
+    before("get accounts", () => {
         return web3.eth.getAccountsPromise()
             .then(_accounts => accounts = _accounts)
             .then(() => web3.version.getNetworkPromise())
             .then(_networkId => {
                 networkId = _networkId;
                 Owned.setNetwork(networkId);
-                console.log(accounts)
+                owner = accounts[0];
+                attacker = accounts[1];
+                testAccount = accounts[2];
             });
     });
 
-    before("deploy owned", function () {
-        return Owned.new({from: accounts[0]})
+    before("deploy owned", () => {
+        return Owned.new({from: owner})
             .then(_owned => owned = _owned)
     });
 
-    before("deploy safemath", function () {
-        return SafeMath.new({from: accounts[0]})
+    before("deploy safemath", () => {
+        return SafeMath.new({from: owner})
             .then(_safeMath => safeMath = _safeMath)
             .then(() => Owned.link({SafeMath: safeMath.address}));
     });
 
-    it("should set accounts[1] as a new candidate", function () {
+    it("should set testAccount as a new candidate", () => {
         return owned.candidateOwner()
-            .then(() => owned.setCandidate(accounts[1], {from: accounts[0]})
+            .then(() => owned.setCandidate(testAccount, {from: owner})
                 .then(() => owned.candidateOwner())
                 .then(candidateOwner => assert.strictEqual(
                     candidateOwner.toString(),
-                    accounts[1],
-                    "should set candidateOwner to accounts[1]")))
+                    testAccount,
+                    "should set candidateOwner to testAccount")))
     });
 
-    it("shouldn't set accounts[1] as a new candidate", function () {
+    it("shouldn't set attacker as a new candidate", () => {
         return owned.candidateOwner()
             .then(() => {
-                return owned.setCandidate(accounts[1], {from: accounts[1]}).should.be.rejected;
+                return owned.setCandidate(attacker, {from: attacker}).should.be.rejected;
             })
-    })
+    });
 
-    it("should set accounts[1] as the new owner", function () {
+    it("should set testAccount as the new owner", () => {
         return owned.owner()
-            .then(() => owned.setCandidate(accounts[1], {from: accounts[0]}))
-            .then(() => owned.getOwnership({from: accounts[1]})
+            .then(() => owned.setCandidate(testAccount, {from: owner}))
+            .then(() => owned.getOwnership({from: testAccount})
                 .then(() => owned.owner())
                 .then(owner => assert.strictEqual(
                     owner.toString(),
-                    accounts[1],
-                    "should set owner to accounts[1]")))
+                    testAccount,
+                    "should set owner to testAccount")))
     })
 });
